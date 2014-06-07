b1587710aeb42facf49516f5e384b005b5ae0985
Stanislaw Klekot <dozzie@jarowit.net>
2014-06-07 21:05:39 +0200

    Added timestamping entries.

--- wlokalu/models.py
+++ wlokalu/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Person(models.Model):
   nick = models.CharField(max_length = 256, primary_key = True)
+  since = models.DateTimeField(auto_now_add = True)
 
 #-----------------------------------------------------------------------------
 # vim:ft=python:nowrap:foldmethod=marker
